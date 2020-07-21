40fe8bd9c93a30bfa71d6ce120cac3c81b593f8c
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2020-07-21 16:52:56 +0700

    style fix

--- data-raw/wachats_kaggle.R
+++ data-raw/wachats_kaggle.R
@@ -4,43 +4,43 @@ library(tidyverse)
 library(lubridate)
 library(glue)
 
-wachats_kaggle_raw <- 
+wachats_kaggle_raw <-
   read_lines("data-raw/datasets_814_1503__chat.txt")
 
-wachats_kaggle <- 
-  wachats_kaggle_raw %>% 
-  str_replace(pattern = "(?<=A|P)M:", "M -") %>% 
-  str_replace("image omitted", "Media omitted") %>% 
-  str_replace("video omitted", "Media omitted") %>% 
-  enframe(name = NULL, value = "content") %>% 
+wachats_kaggle <-
+  wachats_kaggle_raw %>%
+  str_replace(pattern = "(?<=A|P)M:", "M -") %>%
+  str_replace("image omitted", "Media omitted") %>%
+  str_replace("video omitted", "Media omitted") %>%
+  enframe(name = NULL, value = "content") %>%
   separate(
     content,
     into = c("datetime", "content"),
-  sep = "(?<=\\d{1,2}/\\d{1,2}/\\d{2}, \\d{1,2}:\\d{2}:\\d{2} [AP]M) - ",
-  fill = "left"
-) %>% 
+    sep = "(?<=\\d{1,2}/\\d{1,2}/\\d{2}, \\d{1,2}:\\d{2}:\\d{2} [AP]M) - ",
+    fill = "left"
+  ) %>%
   mutate(
     chatid = case_when(
       !is.na(datetime) ~ row_number(),
       TRUE ~ NA_integer_
     )
-  ) %>% 
+  ) %>%
   fill(chatid, datetime) %>%
-  group_by(chatid) %>% 
+  group_by(chatid) %>%
   summarise(
     datetime = unique(datetime),
     content = paste0(content, collapse = "\n")
-  ) %>% 
-  ungroup() %>% 
+  ) %>%
+  ungroup() %>%
   mutate(
     datetime = dmy_hms(datetime),
     datetime = format(datetime, format = "%d/%m/%Y, %H:%M")
-  ) %>% 
+  ) %>%
   transmute(
     content = glue("{datetime} - {content}", .na = NULL)
-  ) %>% 
+  ) %>%
   deframe()
-  
+
 write_lines(wachats_kaggle, "data-raw/wachats_kaggle.txt")
 
 usethis::use_data(wachats_kaggle, overwrite = TRUE, internal = TRUE)
